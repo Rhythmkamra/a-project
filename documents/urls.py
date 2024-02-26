6955db80d6a375b94adcb3c02bbbf6417209aaa5
Rhythmkamra <itsmerhythmk@gmail.com>
2024-02-26 20:46:27 +0530

    added a base file rendered it and created the home

--- documents/urls.py
+++ documents/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for armyproject project.
+URL configuration for learn project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -16,13 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from dms import views
+from base import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
-
-]
-
-
-
+    path('b_1/',views.function1),
+    path('',views.function2),
+    path('riri/',views.function3),
+    path('riri2',views.function4),
+    path('riri3',views.StudentInfo),
+    path('sign_up',views.sign_up),
+    # khali wala main page index page ke liye hai
+] 
