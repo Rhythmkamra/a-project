f39e8e9c2f9d8c90f78303507ec70d405d8740ec
Rhythmkamra <itsmerhythmk@gmail.com>
2024-02-26 17:37:15 +0530

    added

--- armyproject/urls.py
+++ armyproject/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from dms import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('documents/', views.document_list),
 ]
+
+
+
+
--- dms/views.py
+++ dms/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Document
+def document_list(request):
+    documents = Document.objects.all()
+    return render(request, 'index.html', {'documents': documents})
 # Create your views here.
