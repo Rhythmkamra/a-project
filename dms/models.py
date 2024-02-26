8d0d46aee95f60da9c7dac2c8887f4364f459f77
Rhythmkamra <itsmerhythmk@gmail.com>
2024-02-26 18:21:01 +0530

    added

--- dms/models.py
+++ dms/models.py
@@ -1,11 +1,16 @@
+# models.py
+
 from django.db import models
 
 class Document(models.Model):
-    remarks = models.CharField(max_length=255, blank=True)
-    document = models.FileField(upload_to='documents/')
+    title = models.CharField(max_length=255)
+    file = models.FileField(upload_to='documents/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    remarks = models.TextField(blank=True)
+
 
     def __str__(self):
-        return self.document.name
+        return self.title
 
 
 # Create your models here.
