f005f2bf69e86816444023976f051ff9d2aa5c1a
Rhythmkamra <itsmerhythmk@gmail.com>
2024-03-02 10:08:44 +0530

    a

--- dms/forms.py
+++ dms/forms.py
@@ -3,6 +3,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class Signup(UserCreationForm):
+ password1 = forms.CharField(label ='Password',widget=forms.PasswordInput)
+ password2 = forms.CharField(label='Confirm Password(again)',widget=forms.PasswordInput)
  class Meta:
   model = User
   fields = ['username','first_name','last_name','email']
+  labels ={'firs_name':'First_name','last_name':'Last_name','email':'Email'}
